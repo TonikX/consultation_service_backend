27097f9fa20c53e0ce933be79330788cead31c37
elizawetta <lari.x@mail.ru>
2026-03-02 16:13:44 +0300

    add postgres

--- consultapi/settings.py
+++ consultapi/settings.py
@@ -84,9 +84,13 @@ WSGI_APPLICATION = "consultapi.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'consultations',
+        'USER': 'consult_admin',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
