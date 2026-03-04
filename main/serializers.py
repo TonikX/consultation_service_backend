aec988b9c7a5674101b6f024d92a3df059cbede9
andan <317628@niuitmo.ru>
2026-03-04 23:34:30 +0300

    implement program, group, subject and teacher-subject endpoints

--- main/serializers.py
+++ main/serializers.py
@@ -5,4 +5,41 @@ from .models import *
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = '__all__'
+        fields = "__all__"
+
+
+class ProgramSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Program
+        fields = "__all__"
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = "__all__"
+
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = "__all__"
+
+
+class TeacherSubjectsSerializer(serializers.ModelSerializer):
+    subject_id = serializers.PrimaryKeyRelatedField(source="id_subject", read_only=True)
+    subject_name = serializers.CharField(
+        source="id_subject_subject_name", read_only=True
+    )
+    teacher_id = serializers.PrimaryKeyRelatedField(source="id_user", read_only=True)
+    teacher_str = serializers.StringRelatedField(source="id_user", read_only=True)
+
+    class Meta:
+        model = TeacherSubjects
+        fields = [
+            "id_teacher_subject",
+            "subject_id",
+            "subject_name",
+            "teacher_id",
+            "teacher_str",
+        ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from rest_framework import viewsets, permissions
+from .serializers import *
+
+
+class ProgramViewSet(viewsets.ModelViewSet):
+    queryset = Program.objects.all()
+    serializer_class = ProgramSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class SubjectViewSet(viewsets.ModelViewSet):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+
+class TeacherSubjectsViewSet(viewsets.ModelViewSet):
+    queryset = TeacherSubjects.objects.all()
+    serializer_class = TeacherSubjectsSerializer
